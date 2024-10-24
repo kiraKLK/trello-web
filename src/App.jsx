2e9e26d4409528c1291d6099b76a7483f9318175
unknown <admin@MSI.tungvan.local>
2024-10-24 21:28:09 +0700

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-import Stack from '@mui/material/Stack';
-import { pink } from '@mui/material/colors';
-import SvgIcon from '@mui/material/SvgIcon';
-import Button from '@mui/material/Button';
 
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  );
-}
+import Board from "./pages/Boards/_id"
+
+
+
+
+
 function App() {
 
   return (
     <>
-      <AccessAlarm />
-      <ThreeDRotation />
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <Stack direction="row" spacing={3}>
-        <HomeIcon />
-        <HomeIcon color="primary" />
-        <HomeIcon color="secondary" />
-        <HomeIcon color="success" />
-        <HomeIcon color="action" />
-        <HomeIcon color="disabled" />
-        <HomeIcon sx={{ color: pink[500] }} />
-      </Stack>
+     <Board/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx' 
+import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </StrictMode>,
 )
